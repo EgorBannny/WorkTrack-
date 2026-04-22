79bc33b3a6e6bd898356e43ebad4ad856c1cad11
Egor2007 <egorbanny2@gmail.com>
2026-04-22 17:13:58 +0300

    Создал main app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from work-track!")
+import uvicorn
+from fastapi import FastAPI
 
+app = FastAPI()
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", reload=True)
