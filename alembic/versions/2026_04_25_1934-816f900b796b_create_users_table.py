0c26b5767898fabffcd59408cbb85bd62b169b8d
Egor2007 <egorbanny2@gmail.com>
2026-04-25 19:37:59 +0300

    Создал первую версию таблицы пользователей и миграцию к ней

---
+++ alembic/versions/2026_04_25_1934-816f900b796b_create_users_table.py
@@ -0,0 +1,37 @@
+"""create users table
+
+Revision ID: 816f900b796b
+Revises:
+Create Date: 2026-04-25 19:34:30.074534
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "816f900b796b"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=320), nullable=False),
+        sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
--- core/models/user.py
+++ core/models/user.py
@@ -1,7 +1,8 @@
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 from ..models.mixins.itd_id_pk import IntIdPkMixin
 
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
