9ac3dde7d6deb9b6da6b7774f5da59eb0ff54602
Egor2007 <egorbanny2@gmail.com>
2026-04-25 19:46:58 +0300

    Добавлен метод для user database

--- core/models/user.py
+++ core/models/user.py
@@ -1,8 +1,16 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 from ..models.mixins.itd_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
+        return SQLAlchemyUserDatabase(session, User)
