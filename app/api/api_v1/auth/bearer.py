0169e9382dd9949ad44ec51a20e7a0c177d46044
Egor2007 <egorbanny2@gmail.com>
2026-04-28 18:39:32 +0300

    Добавление register ручек для аутентификаций

--- app/api/api_v1/auth/bearer.py
+++ app/api/api_v1/auth/bearer.py
@@ -2,12 +2,24 @@ from fastapi import APIRouter
 from app.core.config.main_config import settings
 from .fastapi_users import fastapi_users
 from app.core.authentication import auth_bearer_backend
+from app.core.schemas import UserRead, UserCreate, UserUpdate
 
 bearer_router = APIRouter(
     prefix=settings.api.v1.prefix.bearer,
     tags=[settings.api.v1.tags.bearer],
 )
 
+# /login /logout
 bearer_router.include_router(
-    router=fastapi_users.get_auth_router(auth_bearer_backend),
+    router=fastapi_users.get_auth_router(
+        backend=auth_bearer_backend,
+    ),
+)
+
+# /register
+bearer_router.include_router(
+    router=fastapi_users.get_register_router(
+        user_schema=UserRead,
+        user_create_schema=UserCreate,
+    )
 )
--- app/api/api_v1/auth/cookie.py
+++ app/api/api_v1/auth/cookie.py
@@ -2,12 +2,24 @@ from fastapi import APIRouter
 from app.core.config.main_config import settings
 from .fastapi_users import fastapi_users
 from app.core.authentication import auth_cookie_backend
+from app.core.schemas import UserRead, UserCreate, UserUpdate
 
 cookie_router = APIRouter(
     prefix=settings.api.v1.prefix.cookie,
     tags=[settings.api.v1.tags.cookie],
 )
 
+# /login /logout
 cookie_router.include_router(
-    router=fastapi_users.get_auth_router(auth_cookie_backend),
+    router=fastapi_users.get_auth_router(
+        backend=auth_cookie_backend,
+    ),
+)
+
+# /register
+cookie_router.include_router(
+    router=fastapi_users.get_register_router(
+        user_schema=UserRead,
+        user_create_schema=UserCreate,
+    )
 )
