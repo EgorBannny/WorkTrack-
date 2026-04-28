b421fe2c0be59d4aba47da53c89cab0a3bbf9ebf
Egor2007 <egorbanny2@gmail.com>
2026-04-28 23:52:52 +0300

    Добавлен HTTPBearer

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 from app.core.config.main_config import settings
 from .auth import auth_router
 from .users import users_router
 
-api_v1_router = APIRouter(prefix=settings.api.v1.prefix.v1)
+http_bearer = HTTPBearer()
+
+api_v1_router = APIRouter(
+    prefix=settings.api.v1.prefix.v1,
+    dependencies=[Depends(http_bearer)],
+)
 api_v1_router.include_router(router=auth_router)
 api_v1_router.include_router(router=users_router)
