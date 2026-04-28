92d4673d4cfc266cc337a82781d175754354b655
Egor2007 <egorbanny2@gmail.com>
2026-04-28 13:57:40 +0300

    Сделал правки с стратегии

--- app/core/authentication/strategy.py
+++ app/core/authentication/strategy.py
@@ -5,7 +5,7 @@ from core.config.main_config import settings
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=settings.auth.jwt.private_key_path.read_text(),
-        lifetime_seconds=3600,
-        algorithm="RS256",
+        lifetime_seconds=settings.auth.jwt.access_token_lifetime_secondss,
+        algorithm=settings.auth.jwt.algorithm,
         public_key=settings.auth.jwt.public_key_path.read_text(),
     )
