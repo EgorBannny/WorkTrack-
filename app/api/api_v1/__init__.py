6521d0f6554e0f476dda61ee35cbb18aad42f052
Egor2007 <egorbanny2@gmail.com>
2026-04-28 16:54:35 +0300

    Заменил название роутеров

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.core.config.main_config import settings
-from .users import users_router
+from .auth import auth_router
 
 api_v1_router = APIRouter(prefix=settings.api.v1.prefix.v1)
-api_v1_router.include_router(router=users_router)
+api_v1_router.include_router(router=auth_router)
