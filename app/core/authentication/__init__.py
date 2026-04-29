53a188ddfe0a6e6617a2596e37fb895ea5c8fc81
Egor2007 <egorbanny2@gmail.com>
2026-04-29 18:15:52 +0300

    Обновление пакета аутентификации в ядре

--- app/core/authentication/__init__.py
+++ app/core/authentication/__init__.py
@@ -1,6 +1,8 @@
 __all__ = (
     "auth_bearer_backend",
     "auth_cookie_backend",
+    "UserManager",
 )
 
 from .backend import auth_bearer_backend, auth_cookie_backend
+from .user_manager import UserManager
