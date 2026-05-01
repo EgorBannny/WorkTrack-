8da30aea288046549c9a206ff4c18d06ef04a85a
Egor2007 <egorbanny2@gmail.com>
2026-05-01 18:08:31 +0300

    Второй этап перехода на uuid

--- alembic/versions/2026_05_01_1805-7bc6db23331f_create_users_table.py
+++ alembic/versions/2026_05_01_1805-7bc6db23331f_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: 816f900b796b
+Revision ID: 7bc6db23331f
 Revises:
-Create Date: 2026-04-25 19:34:30.074534
+Create Date: 2026-05-01 18:05:35.991389
 
 """
 
@@ -10,9 +10,10 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import fastapi_users_db_sqlalchemy
 
 # revision identifiers, used by Alembic.
-revision: str = "816f900b796b"
+revision: str = "7bc6db23331f"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -21,9 +22,10 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "users",
-        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("id", fastapi_users_db_sqlalchemy.generics.GUID(), nullable=False),
         sa.Column("email", sa.String(length=320), nullable=False),
         sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("token_version", sa.Integer(), nullable=False),
         sa.Column("is_active", sa.Boolean(), nullable=False),
         sa.Column("is_superuser", sa.Boolean(), nullable=False),
         sa.Column("is_verified", sa.Boolean(), nullable=False),
