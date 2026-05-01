1879a8d248bb206a9392b2c8cb812e8a09a86cb4
Egor2007 <egorbanny2@gmail.com>
2026-05-01 17:39:09 +0300

    добавлен redis_helper

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,8 +1,10 @@
 __all__ = (
     "db_helper",
+    "redis_helper",
     "Base",
     "User",
 )
 from .db_helper import db_helper
+from .redis_helper import redis_helper
 from .base import Base
 from .user import User
