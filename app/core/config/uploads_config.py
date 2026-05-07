600a27f55e2bd6c4c13fe8a792e3f05f2639495f
Egor2007 <egorbanny2@gmail.com>
2026-05-07 21:04:14 +0300

    Изменил в конфиге формат хранящихся значений для аватарок

--- app/core/config/uploads_config.py
+++ app/core/config/uploads_config.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__name__).parent.parent.parent.parent
 
 
 class AvatarConfig(BaseModel):
-    allowed_types: set[str] = {"image/jpeg", "image/png", "image/webp"}
+    allowed_types: set[str] = {"jpeg", "png", "webp"}
     max_file_size: int = 5 * 1024 * 102
     min_resolution: int = 100
     max_resolution: int = 500
