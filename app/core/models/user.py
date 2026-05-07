92dc25dc1ff625533347019bd7bb658516e45e79
Egor2007 <egorbanny2@gmail.com>
2026-05-07 20:23:57 +0300

    Обновлена модель для пользователей. Теперь она включает ссылку на авотар пользователя и также имя пользователя

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import Integer
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from .base import Base
@@ -11,6 +11,8 @@ if TYPE_CHECKING:
 class User(Base, SQLAlchemyBaseUserTableUUID):
 
     token_version: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
+    display_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    avatar_url: Mapped[str] = mapped_column(String(500), nullable=True)
 
     @classmethod
     def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
