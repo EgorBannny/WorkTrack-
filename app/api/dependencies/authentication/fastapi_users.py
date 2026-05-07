cc4fc06a54c608a1ceb1fa6611c94e97d999feca
Egor2007 <egorbanny2@gmail.com>
2026-05-07 21:35:18 +0300

    Добавил выполеннии функции для проверки аутентификации пользователя

--- app/api/dependencies/authentication/fastapi_users.py
+++ app/api/dependencies/authentication/fastapi_users.py
@@ -1,4 +1,5 @@
 import uuid
+from fastapi import Depends
 from fastapi_users import FastAPIUsers
 from app.api.dependencies.authentication import get_user_manager
 from app.core.models import User
@@ -9,3 +10,5 @@ fastapi_users = FastAPIUsers[User, uuid.UUID](
     get_user_manager,
     [auth_cookie_backend, auth_bearer_backend],
 )
+
+auth_guard = Depends(fastapi_users.current_user(active=True))
