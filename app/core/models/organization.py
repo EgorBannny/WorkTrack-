35ced9e9b7ce9e3c4adaa87714da22ad917a9b31
Egor2007 <egorbanny2@gmail.com>
2026-05-09 05:50:32 +0300

    Создана таблица для организаций и промежуточная таблица для связи с таблицей пользователей

---
+++ app/core/models/organization.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import UUIDPKMixin, TimestampMixin
+
+if TYPE_CHECKING:
+    from .user_organization import Organization
+
+
+class Organization(Base, UUIDPKMixin, TimestampMixin):
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    slug: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+
+    user_organizations: Mapped["Organization"] = relationship(
+        back_populates="organization"
+    )
---
+++ app/core/models/user_organization.py
@@ -0,0 +1,29 @@
+import uuid
+from typing import TYPE_CHECKING
+from sqlalchemy import ForeignKey, String
+from sqlalchemy import Enum as SAEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import TimestampMixin
+from app.enums.organization import OrgRole
+
+if TYPE_CHECKING:
+    from .user import User
+    from .organization import Organization
+
+
+class UserOrganization(Base, TimestampMixin):
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    org_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("organizations.id", ondelete="CASCADE"), primary_key=True
+    )
+    role: Mapped[OrgRole] = mapped_column(SAEnum(OrgRole), nullable=False)
+    position: Mapped[str | None] = mapped_column(String(100), nullable=True)
+
+    user: Mapped["User"] = relationship(back_populates="user_organizations")
+    organization: Mapped["Organization"] = relationship(
+        back_populates="user_organizations"
+    )
