a69d71b0d25efb9b9ae55d4b49fce60a9974aef3
Egor2007 <egorbanny2@gmail.com>
2026-05-09 05:51:10 +0300

    Прокинуты импорты для пакета

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -3,8 +3,12 @@ __all__ = (
     "redis_helper",
     "Base",
     "User",
+    "Organization",
+    "UserOrganization",
 )
 from .db_helper import db_helper
 from .redis_helper import redis_helper
 from .base import Base
 from .user import User
+from .organization import Organization
+from .user_organization import UserOrganization
