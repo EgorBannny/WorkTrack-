c1eadab8248082a6c81903d18b38a5822904c110
Egor2007 <egorbanny2@gmail.com>
2026-05-09 05:50:54 +0300

    Добавлен параметр для связи в таблицу пользователей

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,11 +1,12 @@
 from typing import TYPE_CHECKING
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from .base import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from .user_organization import UserOrganization
 
 
 class User(Base, SQLAlchemyBaseUserTableUUID):
@@ -13,6 +14,7 @@ class User(Base, SQLAlchemyBaseUserTableUUID):
     token_version: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
     display_name: Mapped[str] = mapped_column(String(100), nullable=False)
 
+    user_organizations: Mapped["UserOrganization"] = relationship(back_populates="user")
 
     @classmethod
     def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
