545b6238ea8fa61dc463e7deed90d8ea84f8c670
Egor2007 <egorbanny2@gmail.com>
2026-05-13 20:42:50 +0300

    Добавлены схемы для проектов

---
+++ app/core/schemas/project.py
@@ -0,0 +1,36 @@
+import uuid
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+from .user import UserRead
+
+
+class ProjectCreate(BaseModel):
+    name: str
+    description: str | None = None
+
+
+class ProjectUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+
+
+class ProjectRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
+    org_id: uuid.UUID
+    name: str
+    description: str | None
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+    creator: UserRead | None
+
+
+class UserProjectRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    user_id: uuid.UUID
+    project_id: uuid.UUID
+    created_at: datetime
+    user: UserRead
