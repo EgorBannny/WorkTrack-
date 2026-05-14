25c55d7cfeecce0f58d231ea8e81d564b8924929
Egor2007 <egorbanny2@gmail.com>
2026-05-14 15:13:28 +0300

    Обновлена структура схем организаций

--- app/core/schemas/organization.py
+++ app/core/schemas/organization.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict
 from app.enums import OrgRole
+from .user import UserRead
 
 
 class OrganizationRead(BaseModel):
@@ -24,3 +25,14 @@ class OrganizationCreate(BaseModel):
 
 class OrganizationUpdate(BaseModel):
     name: str | None = None
+
+
+class UserOrganizationRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    user_id: uuid.UUID
+    org_id: uuid.UUID
+    role: OrgRole
+    postition: str | None = None
+    created_at: datetime
+    user: UserRead
