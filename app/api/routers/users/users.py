443bfeebf823a3de72cdd89506b08e1a142aaaf8
Egor2007 <egorbanny2@gmail.com>
2026-05-24 22:05:35 +0300

    Перенос DELETE /me до include_router fastapi-users

--- app/api/routers/users/users.py
+++ app/api/routers/users/users.py
@@ -1,41 +1,19 @@
-# utils
 from typing import Annotated
 
-# sqlalchemy
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# core
-from app.core.models import db_helper, User
+from app.api.crud.users import deactivate_user
+from app.api.dependencies.authentication import auth_guard, fastapi_users
 from app.core.config import settings
+from app.core.models import db_helper, User
 from app.core.schemas import UserRead, UserUpdate
 
-# api
-from app.api.crud.users import deactivate_user
-from app.api.dependencies.authentication import (
-    auth_guard,
-    fastapi_users,
-)
-
-# fastapi
-from fastapi import APIRouter, Depends, status
-
-# inside the package
 from .avatars import avatars_router
 
 users_router = APIRouter(
     prefix=settings.api.prefix.users,
     tags=[settings.api.tags.users],
-)
-
-users_router.include_router(
-    router=fastapi_users.get_users_router(
-        user_schema=UserRead,
-        user_update_schema=UserUpdate,
-    ),
-)
-
-users_router.include_router(
-    router=avatars_router,
 )
 
 
@@ -48,3 +26,15 @@ async def delete_own_account(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
     await deactivate_user(session, current_user)
+
+
+users_router.include_router(
+    router=fastapi_users.get_users_router(
+        user_schema=UserRead,
+        user_update_schema=UserUpdate,
+    ),
+)
+
+users_router.include_router(
+    router=avatars_router,
+)
